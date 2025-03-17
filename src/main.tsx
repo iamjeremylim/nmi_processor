4823e02575140ddb4bb96243171f7ce2b26ac551
iamjeremylim <limjeremy29@gmail.com>
2025-03-17 14:51:37 +0800

    chore: new folder for global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import GlobalStyles from "./components/GlobalStyles";
+import GlobalStyles from "./styles/GlobalStyles";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
